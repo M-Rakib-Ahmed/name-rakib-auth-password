64b687908f9f5fc243e0abf6cface72a66cfcc80
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2025-01-24 14:41:03 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='btn btn-error'>My Name is Rakib What your name</h1>
+    </div>
+  );
+};
+
+export default App;
